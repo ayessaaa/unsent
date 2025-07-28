5c39755c2c0a67cbb01061888d6647efc3b9c9de
Ayessa <ayessamaedeguzman78@gmail.com>
2025-07-28 17:45:45 +0800

    socket rooms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,18 +7,32 @@ const socket = io.connect("http://localhost:3000");
 function App() {
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
+  const [room, setRoom] = useState("");
   const sendMessage = () => {
-    socket.emit("send_message", { message });
+    socket.emit("send_message", { message, room });
   };
+
+  const joinRoom = () =>{
+    if (room !== ""){
+      socket.emit("join_room", room);
+    }
+  }
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
       setMessageReceived(data.message);
     });
-  }, [socket]);
+  }, []);
 
   return (
     <>
+    <input
+        placeholder="room"
+        onChange={(e) => {
+          setRoom(e.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>join room</button>
       <input
         placeholder="message"
         onChange={(e) => {
